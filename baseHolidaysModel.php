be3c8fb8a45e64a8f655a2928419afc0c99cf201
Robledo <robledocampos@users.noreply.github.com>
2024-04-01 19:33:18 -0300

    Create baseHolidaysModel.php

---
+++ baseHolidaysModel.php
@@ -0,0 +1,31 @@
+<?php
+class baseHolidaysModel {
+
+  public const WEEKDAYS = [
+    "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"
+  ];
+  public string $coverage;
+  public string $year;
+  public array $holidays = [];
+  
+  function __construct(string $year = null, string $coverage) {
+    $year = !is_null($year) ?: date("Y");
+    $this->year = $year;
+    $this->coverage = $coverage;
+  }
+  
+  function buildHoliday(string $date, string $holidayName) array {
+    return [
+      "date" => $date,
+      "name" => $holidayName,
+      "weekday" => $this->weekDay($date),
+      "coverage" => $this->coverage
+    ];
+  }
+  
+  function weekDay(string $date) string {
+    $index = date("w", strtotime($date));
+    return $this::WEEKDAYS[$index];
+  }
+  
+}
