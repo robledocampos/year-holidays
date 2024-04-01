3bd3cecdc8c984dedc813842688de96087513dad
Robledo <robledocampos@users.noreply.github.com>
2024-03-31 23:20:14 -0300

    Update brazilHolidays.php

--- brazilHolidays.php
+++ brazilHolidays.php
@@ -1,6 +1,6 @@
 <?php
 
-class brazilHolidays extend worldHolidays {
+class brazilHolidays extends worldHolidays {
   
   function __construct(string $year) {
     parent::__construct($year);
@@ -10,52 +10,51 @@ class brazilHolidays extend worldHolidays {
   function listBrazilHolidays() {
     $this->holidays["main"][] = $this->carnaval();
     $this->holidays["main"][] = $this->paixao();
-		$this->holidays["main"][] = $this->tiraDentes();
-		$this->holidays["main"][] = $this->corpusChristi();
-		$this->holidays["main"][] = $this->finados();
-		$this->holidays["main"][] = $this->proclamacaoDaRepublica();
+	$this->holidays["main"][] = $this->tiraDentes();
+	$this->holidays["main"][] = $this->corpusChristi();
+	$this->holidays["main"][] = $this->finados();
+	$this->holidays["main"][] = $this->proclamacaoDaRepublica();
   }
 
   function carnaval() {
-	  $carnaval = date_sub(
-		  date_create(
-			  $this->easter()
-		  ), 
-		  date_interval_create_from_date_string('47 days')
+	$carnaval = date_sub(
+	  date_create(
+		$this->easter()
+		), 
+	  	date_interval_create_from_date_string('47 days')
 	  );
 	  return date_format($carnaval, 'Y-m-d');
   }
 
   function paixao() {
 	  $friday = date_sub(
-		  date_create(
-			  $this->easter()
-		  ), 
-		  date_interval_create_from_date_string('2 days')
+	  	date_create(
+		  $this->easter()
+	  	), 
+	  	date_interval_create_from_date_string('2 days')
 	  );
 	  return date_format($friday, 'Y-m-d');
   }
 
   function tiraDentes() {
-	  return $this->year."-04-21";
+  	return $this->year."-04-21";
   }
 
   function corpusChristi() {
-	  $corpusChristi = date_add(
-		  date_create(
-			  $this->easter()
-		  ), 
-		  date_interval_create_from_date_string('60 days')
-	  );
-	  return date_format($corpusChristi, 'Y-m-d');
+	$corpusChristi = date_add(
+		date_create(
+		  $this->easter()
+	  	), 
+	  	date_interval_create_from_date_string('60 days')
+  	);
+  	return date_format($corpusChristi, 'Y-m-d');
   }
 
   function finados() {
-	  return $this->year."-11-02";
+  	return $this->year."-11-02";
   }
 
   function proclamacaoDaRepublica() {
-	  return $this->year."-11-15";
+  	return $this->year."-11-15";
   }
-
 }
