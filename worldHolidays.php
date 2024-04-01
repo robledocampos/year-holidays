047fa67653bc654985da9220cb90f5171074e95a
Robledo <robledocampos@users.noreply.github.com>
2024-04-01 19:54:23 -0300

    Update worldHolidays.php

--- worldHolidays.php
+++ worldHolidays.php
@@ -1,31 +1,22 @@
 <?php
 
-class worldHolidays {
+class worldHolidaysModel extends baseHolidaysModel {
 
-  public const WEEKDAYS = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-  public string $coverage;
-  public string $year;
   public array $universalFraternization;
   public array $easter;
   public array $laborDay;
   public array $christmas;
-  public array $holidays = [];
   
   function __construct(string $year = null) {
     $year = !is_null($year) ?: date("Y");
-    $this->year = $year;
-    $this->coverage = "World";
-    $this->universalFraternization = $this->universalFraternization();
-    $this->easter = $this->easter();
-    $this->laborDay = $this->laborDay();
-    $this->christmas = $this->christmas();
+    parent::__construct($year, "World");
     $this->buildWorldHolidays();
   }
-
+  
   function universalFraternization() {
     return $this->buildHoliday($this->year."-01-01", "universal fraternization");
   }
-
+  
   function easter() {
     return $this->buildHoliday(date("Y-m-d", easter_date($this->year)), "easter");
   }
@@ -44,22 +35,7 @@ class worldHolidays {
     $dates[$this->laborDay["date"]] = $this->laborDay;
     $dates[$this->christmas["date"]] = $this->christmas;
     $this->holidays['main'] = $dates;
-    ksort( $this->holidays['main']);
-  }
-
-  function buildHoliday(string $date, string $holidayName) {
-    return [
-      "date" => $date,
-      "name" => $holidayName,
-      "weekday" => $this->weekDay($date),
-      "coverage" => $this->coverage
-    ];
-  }
-  
-  function weekDay(string $date) {
-    $index = date("w", strtotime($date));
-    return $this::WEEKDAYS[$index];
+    ksort($this->holidays['main']);
   }
   
 }
-
